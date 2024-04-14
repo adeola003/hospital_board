18b7e02a365a92704887ce692b84010b2fc87968
adeola003 <khadaf3@gmail.com>
2024-04-14 21:52:25 +0100

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   enum role: { receptionist: 0, doctor: 1 }
+  # Validation for name and role presence
+  validates :name, presence: true
+  validates :role, presence: true
+
+  # Validation for doctor's specialization
+  validates :specialty, presence: true, if: -> { doctor? }
 end
